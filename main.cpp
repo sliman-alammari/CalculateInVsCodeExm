1b44d3874b8706d4119566f296f507a6c9c01a2a
sliman alammari <sliman77ammari@gmail.com>
2026-09-21 14:05:18 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    int x, y;
+    char op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> x;
+
+    std::cout << "Enter second number: ";
+    std::cin >> y;
+
+    std::cout << "Choose operation (+ or -): ";
+    std::cin >> op;
+
+    if (op == '+') {
+        std::cout << "Result: " << sum(x, y) << std::endl;
+    } else if (op == '-') {
+        std::cout << "Result: " << subtract(x, y) << std::endl;
+    } else {
+        std::cout << "Invalid operation!" << std::endl;
+    }
+
+    return 0;
+}
